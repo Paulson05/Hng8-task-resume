129a54b6ccb19265d6b16c91c1e30ecad0f1d143
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-08-17 17:34:25 +0100

    backend dashboard added

---
+++ resources/views/resume.blade.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title> Industrial & Factory Bootstrap 5 Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="p2/img/favicon.ico">
+    <!-- All CSS Files -->
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Icon Font -->
+    <link rel="stylesheet" href="css/font.awesome.min.css">
+    <!-- Icon Font -->
+    <link rel="stylesheet" href="css/et-line.css">
+    <!-- Plugins css file -->
+    <link rel="stylesheet" href="css/plugins.css">
+    <!-- Main style -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Responsive css -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Modernizr JS -->
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '../../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5JCTSSF');</script>
+    <!-- End Google Tag Manager -->
+</head>
+<style>
+    .hero-section{
+        background-image: url('img/download.png') !important;
+    }
+</style>
+<body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JCTSSF"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<!-- Start Header Section -->
+<div class="header-section section sticker">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="header-top">
+                    <!-- Logo -->
+                    <div class="logo">
+                        <a class="non-sticky" href="induste/index.html"><img style="width: 100%; height: 60px;" src="img/hng2.png" alt=""></a>
+                        <a href="induste/index.html" class="sticky-logo"><img src="img/white-logo.png" alt=""></a>
+                    </div>
+                    <!-- Logo -->
+                    <div class="button-buy">
+                        <a class="buy-btn top-btn" target="_blank" href="#">Contact us</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Header Section End -->
+<!-- Start Hero Section -->
+<div class="hero-section section overlay" style="background-image: url('img/download.png')">
+    <div class="container">
+        <div class="row">
+            <div class="hero-content text-center col-12">
+                <h1><strong>Welcome to hot3!s.ng</strong>HNG8 intership  </h1>
+                <a class="buy-btn" href="#demo">Apply Now</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- End Footer Section -->
+<!-- Placed JS at the end of the document so the pages load faster -->
+<!-- jQuery latest version -->
+<script src="js/vendor/jquery-3.1.1.min.js"></script>
+<!-- Bootstrap js -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Plugins js -->
+<script src="js/plugins.js"></script>
+<!-- Ajax Mail js -->
+<script src="js/ajax-mail.js"></script>
+<!-- Main js -->
+<script src="js/main.js"></script>
+</body>
+
+<!-- Mirrored from htmldemo.hasthemes.com/induste/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 19 May 2021 10:09:29 GMT -->
+</html>
